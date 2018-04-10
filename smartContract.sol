f47ac11a762fd02b6a9c630c8c01fd87a8d123c9
ljunokas <ljunokas@gmail.com>
2018-04-10 11:09:59 +0300

    Update smartContract.sol

--- smartContract.sol
+++ smartContract.sol
@@ -1,17 +1,18 @@
 pragma solidity ^0.4.0;
 contract TokenInterface {
-    function transfer(address _from, address _to, uint _value) returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
 }
 
 contract GatepoolContract {
     address owner;
     address contractAddress = this;
-
+    
     // token contract address
     address tokenAddress;
     // ICO address where ehters must be transfered
     address icoAddress;
-
+    
     // contract balance
     uint value;
     uint constant hardCap = 10 ether;
@@ -19,15 +20,16 @@ contract GatepoolContract {
     uint constant ratio = hardCapTotalTokenSupply/hardCap;
     uint constant houseEtherFeePercent = 10;
     uint constant houseTokenFeePercent = 10;
-
-
-    bool allowance = false;
-
+    
+    
+    bool releaseTokens = false;
+    bool releaseEther = false;
+    
     // token contract instance
     TokenInterface private _instance;
-
+    
     mapping (address => bool) userAddr;
-
+    
     struct Investor {
         uint maxValue;
         uint invested;
@@ -35,7 +37,7 @@ contract GatepoolContract {
     }
 
     Investor myInvestor;
-
+    
     mapping (address => Investor) _investors;
 
     function GatepoolContract() public {
@@ -47,22 +49,22 @@ contract GatepoolContract {
         require(msg.sender == owner);
         _;
     }
-
+    
      modifier individualCap() {
         require(_investors[msg.sender].maxValue <= _investors[msg.sender].invested + msg.value);
         _;
     }
-
+    
     modifier onlyWhitelisted() {
         require(userAddr[msg.sender]);
         _;
     }
-
+    
     modifier hardCapMod() {
         require(msg.value + value <= hardCap);
         _;
     }
-
+    
     // only owner of contract can whitelist the address
     function whitelistAddress (address user, uint maxVal) public onlyOwner {
         userAddr[user] = true;
@@ -70,40 +72,63 @@ contract GatepoolContract {
         _investors[user].invested = 0;
         _investors[user].claimed = false;
     }
-
+    
     // owner should set token contract instance when available
     function setContractInstance(address _address) public onlyOwner {
         tokenAddress = _address;
         _instance = TokenInterface(address(_address));
     }
-
+    
     function setIcoAddress(address _address) public onlyOwner {
         icoAddress = _address;
     }
-
-    // owner makes the transfer to ico addrens
+    
+    // owner makes the transfer to ico address
     function sendEtherToIco () public onlyOwner returns (bool) {
         icoAddress.transfer(value);
         return true;
     }
-
+    
     function claimTokens () public onlyWhitelisted returns (bool){
+        require(!_investors[msg.sender].claimed);
         uint tokensToBeClaimed =  _investors[msg.sender].invested * ratio;
-        if(_instance.transfer(contractAddress, msg.sender, tokensToBeClaimed)){
+        _instance.approve(msg.sender, tokensToBeClaimed);
+        if(_instance.transferFrom(contractAddress, msg.sender, tokensToBeClaimed)){
              _investors[msg.sender].claimed = true;
+              return true;
         } else {
             _investors[msg.sender].claimed = false;
+             return false;
         }
+       
+    }
+    
+    function initiateReleaseEther() public onlyOwner {
+        releaseEther = true;
+    }
+    
+    function withdrawEther() public onlyWhitelisted returns (bool) {
+        require(releaseEther);
+        require(userAddr[msg.sender]);
+        require(_investors[msg.sender].invested > 0);
+        
+        msg.sender.transfer(_investors[msg.sender].invested);
+        _investors[msg.sender].invested = 0;
+        
         return true;
     }
-
+    
     function () payable public hardCapMod onlyWhitelisted individualCap {
         value += msg.value;
         _investors[msg.sender].invested += msg.value;
     }
-
+    
+    
+    
     function contractBalance () public returns (uint) {
         return contractAddress.balance;
     }
-
+    
 }
+
+
